2e4964f4fb04669540dbf2ed1ef485f18e4327c3
Lucas Dias <272644786+lucassdias23@users.noreply.github.com>
2026-09-07 22:55:28 -0300

    feat: exercício de POO - máquina de dar banho em pet, em um sistema de pet shop.

---
+++ src/programacao_poo/banhoPet.java
@@ -0,0 +1,112 @@
+package programacao_poo;
+
+import java.util.Scanner;
+
+//Escreva um código onde temos o controle de banho de um petshop, a maquina de banhos dos pets deve ter as seguintes operações:
+
+//Dar banho no pet;
+//Abastecer com água;
+//Abastecer com shampoo;
+//Verificar nivel de água;
+//Verificar nivel de shampoo;
+//Verificar se tem pet no banho;
+//Colocar pet na maquina;
+//Retirar pet da máquina;
+//Limpar maquina.
+
+
+//Siga as seguintes regras para implementação:
+
+//A maquina de banho deve permitir somente 1 pet por vez;
+//Cada banho realizado irá consumir 10 litros de água e 2 litros de shampoo;
+//A máquina tem capacidade máxima de 30 litros de água e 10 litros de shampoo;
+//Se o pet for retirado da maquina sem estar limpo será necessário limpar a máquina para permitir a entrada de outro pet;
+//A limpeza da máquina ira consumir 3 litros de água e 1 litro de shampoo;
+//O abastecimento de água e shampoo deve permitir 2 litros por vez que for acionado;
+
+public class banhoPet {
+
+    private static String WELCOME_MESSAGE = "Olá, bem vindo ao serviço de Banho & Tosa \n \nEscolha uma das opções: \n";
+
+    private final static Scanner scanner = new Scanner(System.in);
+
+    private final static PetMachine petMachine = new PetMachine();
+
+    public static void main(String[] args) {
+
+        System.out.println(WELCOME_MESSAGE);
+
+        scanner.useDelimiter("\\n");
+        var option = -1;
+
+        do {
+            System.out.println("1. Dar banho no pet.");
+            System.out.println("2. Abastecer a máquina com água.");
+            System.out.println("3. Abastecer a máquina com shampoo.");
+            System.out.println("4. Verificar nível de água.");
+            System.out.println("5. Verificar nível de shampoo.");
+            System.out.println("6. Verificar se tem pet no banho.");
+            System.out.println("7. Colocar pet na máquina.");
+            System.out.println("8. Retirar pet da máquina.");
+            System.out.println("9. Limpar máquina.");
+            System.out.println("0. Sair.");
+            option = scanner.nextInt();
+
+            switch (option) {
+                case 1 -> petMachine.takeAShower();
+                case 2 -> setWater();
+                case 3 -> setShampoo();
+                case 4 -> verifyWater();
+                case 5 -> verifyShampoo();
+                case 6 -> checkIfHasPetInMachine();
+                case 7 -> setPetInPetMachine();
+                case 8 -> petMachine.removePet();
+                case 9 -> petMachine.wash();
+                case 0 -> System.exit(0);
+                default -> System.out.println("Opção inválida.");
+            }
+        }
+
+        while (option != 0);
+    }
+
+    private static void setWater() {
+        System.out.println("Tentando colocar água na máquina");
+        petMachine.addWater();
+    }
+
+    private static void setShampoo() {
+        System.out.println("Tentando colocar shampoo na máquina");
+        petMachine.addShampoo();
+    }
+
+    private static void verifyWater() {
+        var amount = petMachine.getWater();
+        System.out.println("A máquina está no momento com " + amount + " litros de água.");
+    }
+
+    private static void verifyShampoo() {
+        var amount = petMachine.getShampoo();
+        System.out.println("A máquina está no momento com " + amount + " litros de shampoo.");
+    }
+
+    private static void checkIfHasPetInMachine() {
+        var hasPet = petMachine.hasPet();
+        System.out.println(hasPet ? "Tem pet na máquina" : "Não tem pet na máquina.");
+    }
+
+    public static void setPetInPetMachine() {
+
+        var name = "";
+        while (name == null || name.isEmpty()) {
+            System.out.println("Informe o nome do pet: ");
+            name = scanner.next();
+        }
+
+        var pet = new Pet(name);
+        petMachine.setPet(pet);
+
+
+    }
+
+}
